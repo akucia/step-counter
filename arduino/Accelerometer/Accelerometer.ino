9d8de929774ac5450e11d318c7f00ef3811b35b0
akuc <17779555+akucia@users.noreply.github.com>
2023-08-13 14:21:25 +0200

    read data from arduino via bluetooth

--- arduino/Accelerometer/Accelerometer.ino
+++ arduino/Accelerometer/Accelerometer.ino
@@ -6,13 +6,20 @@ const char *deviceServiceUuid = "96b1c8ed-fd4b-4bc3-b5da-9e3ed654f1b1";
 const char *deviceServiceCharacteristicUuid = "551de921-bbaa-4e0a-9374-3e30e88a9073";
 
 BLEService accelerometerService(deviceServiceUuid);
-BLEByteCharacteristic accelerometerCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWrite);
+BLECharacteristic accelerometerCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWrite, 12);
+
+union AccelerometerData {
+    float values[3];
+    unsigned char bytes[12];
+};
+
+const unsigned char initializerAcc[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 void setup() {
     Serial.begin(9600);
-    digitalWrite(LED_BUILTIN, LOW);
-    while (!Serial)
-        ;
+    digitalWrite(LED_BUILTIN, HIGH);
+    //    while (!Serial)
+    //        ;
     Serial.println("Started");
 
     // Setting up bluetooth connection
@@ -21,13 +28,13 @@ void setup() {
         while (1)
             ;
     }
-
+    BLE.setDeviceName("Arduino Nano 33 BLE (Accelerometer)");
     BLE.setLocalName("Arduino Nano 33 BLE (Accelerometer)");
     // assign service and characteristic to bluetooth
     BLE.setAdvertisedService(accelerometerService);
     accelerometerService.addCharacteristic(accelerometerCharacteristic);
     BLE.addService(accelerometerService);
-    accelerometerCharacteristic.writeValue(-1);
+    accelerometerCharacteristic.writeValue(initializerAcc, 12);
     BLE.advertise();
 
     // Set up accelerometer
@@ -48,8 +55,8 @@ void setup() {
 void loop() {
 
     BLEDevice central = BLE.central();
+    digitalWrite(LED_BUILTIN, HIGH);
     Serial.println("- Discovering central device...");
-    delay(500);
 
     if (central) {
         Serial.println("* Connected to central device!");
@@ -57,19 +64,30 @@ void loop() {
         Serial.println(central.address());
         Serial.println(" ");
         while (central.connected()) {
-            if (accelerometerCharacteristic.written()) {
-                writeAccelerometer();
-            };
+            digitalWrite(LED_BUILTIN, HIGH);
+            AccelerometerData data = getAccelerometer();
+            unsigned char *acc = (unsigned char *)&data;
+            int success = accelerometerCharacteristic.writeValue(acc, 12);
+            if (!success) {
+                Serial.println("value not written");
+            }
+            digitalWrite(LED_BUILTIN, LOW);
+            delay(10);
         }
         Serial.println("* Disconnected from central device!");
     }
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+}
+bool setValue(const unsigned char value[], unsigned short length) {
+    Serial.println("setValue");
+    return true;
 }
 
-void writeAccelerometer() {
-    float x, y, z;
-
+AccelerometerData getAccelerometer() {
+    float x = 0, y = 0, z = 0;
     if (IMU.accelerationAvailable()) {
-        digitalWrite(LED_BUILTIN, HIGH);
 
         IMU.readAcceleration(x, y, z);
         Serial.print(millis());
@@ -79,8 +97,10 @@ void writeAccelerometer() {
         Serial.print(y);
         Serial.print('\t');
         Serial.println(z);
-        digitalWrite(LED_BUILTIN, LOW);
-
-        delay(100);
     }
+    union AccelerometerData data;
+    data.values[0] = x;
+    data.values[1] = y;
+    data.values[2] = z;
+    return data;
 }
