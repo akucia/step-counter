dc41feae8d7b6ea266924450233d8a992bf69754
akuc <17779555+akucia@users.noreply.github.com>
2023-10-25 21:52:45 +0200

    add mocks for devices

---
+++ scripts/connect_and_plot.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+import datetime
+import os
+import queue
+from functools import partial
+
+from bleak import BleakScanner
+from bokeh.document import without_document_lock
+from bokeh.events import ButtonClick
+from bokeh.layouts import column, row
+from bokeh.models import Button, ColumnDataSource
+from bokeh.plotting import curdoc, figure
+
+from step_counter.data_sources import BLESource, DummySource, MockSource, Source
+
+PLOT_ROLLOVER = 200
+# TODO change this global counter to something better
+GLOBAL_PLOT_INDEX = 0
+
+source_x = ColumnDataSource(data=dict(x=[0], y=[0]))
+source_y = ColumnDataSource(data=dict(x=[0], y=[0]))
+source_z = ColumnDataSource(data=dict(x=[0], y=[0]))
+source_b = ColumnDataSource(data=dict(x=[0], y=[0]))
+
+doc = curdoc()
+data_queue = queue.SimpleQueue()
+
+
+async def set_title(title):
+    p1.title.text = title
+
+
+# TODO convert all of these 3 functions into one
+@without_document_lock
+async def use_ble_source():
+    service_uuid = os.environ["SERVICE_UUID"]
+    device_uuid = os.environ["DEVICE_UUID"]
+    doc.add_next_tick_callback(partial(set_title, title="Scanning..."))
+    source = None
+    while source is None:
+        print(f"Scanning for source with UUID {device_uuid}")
+        devices = await BleakScanner.discover()
+        selected_device = [d for d in devices if device_uuid in d.metadata["uuids"]]
+        if not selected_device:
+            print(f"Device with UUID {device_uuid} not found")
+            doc.add_next_tick_callback(partial(set_title, title="Device not found"))
+        else:
+            print(f"Device with UUID {device_uuid} found")
+            doc.add_next_tick_callback(partial(set_title, title="Device found"))
+            source = BLESource(selected_device[0], service_uuid)
+            print(source)
+            await read_data_from_source(source)
+
+
+@without_document_lock
+async def use_dummy_source():
+    doc.add_next_tick_callback(partial(set_title, title="Scanning..."))
+    source = None
+    while source is None:
+        doc.add_next_tick_callback(partial(set_title, title="Device found"))
+        source = DummySource()
+        await read_data_from_source(source)
+
+
+@without_document_lock
+async def use_mock_source():
+    doc.add_next_tick_callback(partial(set_title, title="Scanning..."))
+    source = None
+    while source is None:
+        doc.add_next_tick_callback(partial(set_title, title="Device found"))
+        source = MockSource(
+            "data/steps/train/accelerometer-data-2023-08-25 20:31:42.558559.csv"
+        )
+        await read_data_from_source(source)
+
+
+def read_queue_and_save_data():
+    filename = f"accelerometer-data-{datetime.datetime.now()}.csv"
+    with open(filename, "w") as f:
+        f.write("timestamp,x,y,z,button_state\n")
+    counter = 0
+    while True:
+        try:
+            timestamp, data = data_queue.get_nowait()
+            with open(filename, "a") as f:
+                print(timestamp, *data, sep=",", file=f)
+            counter += 1
+        except queue.Empty:
+            print(f"Queue empty, read {counter} items")
+            return
+
+
+@without_document_lock
+async def read_data_from_source(source: Source):
+    async for timestamp, accelerometer_data in source.read_data():
+        data_queue.put((timestamp, accelerometer_data))
+        doc.add_next_tick_callback(
+            partial(
+                update_plot,
+                x=accelerometer_data[0],
+                y=accelerometer_data[1],
+                z=accelerometer_data[2],
+                button_state=accelerometer_data[3],
+            )
+        )
+
+
+async def _update_plot_source(source: ColumnDataSource, data: float):
+    source.stream(
+        new_data={
+            "x": [source.data["x"][-1] + 1],
+            "y": [data],
+        },
+        rollover=PLOT_ROLLOVER,
+    )
+
+
+async def update_plot(x: float, y: float, z: float, button_state: float):
+    # todo find a way to get index better than global counter
+    global GLOBAL_PLOT_INDEX
+    GLOBAL_PLOT_INDEX += 1
+
+    # TODO make rollover an argument
+    await _update_plot_source(source_x, x)
+    await _update_plot_source(source_y, y)
+    await _update_plot_source(source_z, z)
+    await _update_plot_source(source_b, button_state)
+
+
+p1 = figure(y_range=[-4, 4])
+p1.title.text = "..."
+lx = p1.line(x="x", y="y", source=source_x, color="red")
+ly = p1.line(x="x", y="y", source=source_y, color="green")
+lz = p1.line(x="x", y="y", source=source_z, color="blue")
+
+p2 = figure(y_range=[-0.1, 1.5])
+
+lb = p2.line(x="x", y="y", source=source_b, color="blue")
+
+button = Button(label="Save data to file")
+
+button.on_event(ButtonClick, read_queue_and_save_data)
+
+doc.add_root(column(row(p1, p2), button))
+doc.add_next_tick_callback(use_mock_source)
+
+# TODO add button to start recording to file
---
+++ src/step_counter/data_sources.py
@@ -0,0 +1,97 @@
+"""Accelerator data sources for step counter.
+
+This module contains classes that can be used to read data from BLE device or mocked data sources
+"""
+
+
+import abc
+import asyncio
+import itertools
+import time
+from pathlib import Path
+from typing import AsyncIterable, Tuple, Union
+
+import numpy as np
+from bleak import BleakClient, BLEDevice
+
+
+class Source(abc.ABC):
+    """Abstract base class for accelerator data sources."""
+
+    @abc.abstractmethod
+    def read_data(self) -> AsyncIterable[Tuple[float, np.ndarray]]:
+        """Read data from the source."""
+        pass
+
+
+class DummySource(Source):
+    """Dummy data source that generates random data."""
+
+    def __init__(self, seed: int = 42):
+        """Initialize the random number generator.
+
+        Args:
+            seed: a seed for the random number generator
+
+        """
+        np.random.seed(seed)
+
+    async def read_data(self) -> AsyncIterable[Tuple[float, np.ndarray]]:
+        """Generate random data indefinitely."""
+        while True:
+            await asyncio.sleep(0.1)
+            timestamp = time.time()
+            data_xyz = np.random.random(3)
+            data_button = np.random.randint(0, 2)
+            yield timestamp, np.concatenate((data_xyz, [data_button]))
+
+
+class MockSource(Source):
+    def __init__(self, file: Union[str, Path]):
+        """Mock data source that reads data from a file.
+
+        Args:
+            file: a path to a csv file with data
+
+        Notes:
+            The file should have a header and the following columns:
+            timestamp, x, y, z, button_state
+        """
+        self.file = file
+
+    async def read_data(self) -> AsyncIterable[Tuple[float, np.ndarray]]:
+        """Read data from the file indefinitely."""
+
+        with open(self.file) as f:
+            all_lines = f.readlines()[1:]
+
+        previous_timestamp, *_ = all_lines[1].split(",")
+        previous_timestamp = float(previous_timestamp)
+        # loop over the data indefinitely
+        for line in itertools.cycle(all_lines):
+            await asyncio.sleep(0.0)
+            timestamp, x, y, z, button_state = line.split(",")
+            timestamp = float(timestamp)
+            data = np.array([float(x), float(y), float(z), float(button_state)])
+            yield timestamp, data
+            time_diff = timestamp - previous_timestamp
+            previous_timestamp = timestamp
+            # pause exact amount of time between two timestamps read from file to simulate real-time data
+            await asyncio.sleep(time_diff)
+
+
+class BLESource(Source):
+    """BLE data source that reads data from a BLE device."""
+
+    def __init__(self, device: BLEDevice, service_uuid: str):
+        self.device = device
+        self.service_uuid = service_uuid
+
+    async def read_data(self) -> AsyncIterable[Tuple[float, np.ndarray]]:
+        """Read data from the BLE device indefinitely."""
+        async with BleakClient(self.device.address) as client:
+            while client.is_connected:
+                bytes_data = await client.read_gatt_char(self.service_uuid)
+                timestamp = time.time()
+                decoded_data = np.frombuffer(bytes_data, dtype=np.float32)
+                yield timestamp, decoded_data
